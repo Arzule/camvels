05fc1018afea41437f19d7660efe56098bd8ee30
Arzule <gerardocarpio1000365@gmail.com>
2025-11-25 00:14:07 -0500

    Add Conexion.java

---
+++ src/main/java/util/Conexion.java
@@ -0,0 +1,16 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+    private static final String URL = System.getenv("DB_URL");
+    private static final String USER = System.getenv("DB_USER");
+    private static final String PASS = System.getenv("DB_PASS");
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
+}
